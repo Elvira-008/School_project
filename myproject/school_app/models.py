22676b836ac1e9b585c04443a4d0fb9f1ef5458f
d1ana08 <jolchubaevadiana@gmail.com>
2026-05-30 16:39:52 +0600

    diana

--- myproject/school_app/models.py
+++ myproject/school_app/models.py
@@ -1,52 +1,25 @@
+from datetime import timezone
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-
-ROLE_CHOICES=(
-("student","student"),
-("teacher","teacher"),
-("admin","admin"),
-)
-
 class UserProfile(AbstractUser):
-    photo_profile = models.ImageField(upload_to='photo_profile/')
+    photo_profile = models.ImageField(upload_to='photo_profile/', null=True, blank=True)
+    ROLE_CHOICES = (
+        ("student", "student"),
+        ("teacher", "teacher"),
+        ("admin", "admin"))
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="student")
     full_name = models.CharField(max_length=150)
     phone_number = PhoneNumberField(null=True, blank=True)
-<<<<<<< HEAD
-<<<<<<< HEAD
-    login = models.CharField(max_length=64)
-    create_register = models.DateTimeField(auto_now_add=True)#черновик
-=======
-    email = models.EmailField(null=True, blank=True)
-    login = models.CharField(max_length=64)
-    create_register = models.DateTimeField(auto_now_add=True)
->>>>>>> a3f4685408f1182b68358eb3169d9d32d815c96d
-=======
-    login = models.CharField(max_length=64)
-    create_register = models.DateTimeField(auto_now_add=True)#черновик
->>>>>>> d8dbb0b8e4e4a42dcd5c08c747ea8a8f173c46c9
+    login = models.CharField(max_length=64, unique=True)
+    create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.full_name}, {self.role}'
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    def save(self, *args, **kwargs):
-        if self.password.startswith("S-"):
-            self.role = "teacher"
-        elif self.password.startswith("A-"):
-            self.role = "admin"
-        else:
-            self.role = "student"
-        super().save(*args, **kwargs)
-
->>>>>>> a3f4685408f1182b68358eb3169d9d32d815c96d
-=======
->>>>>>> d8dbb0b8e4e4a42dcd5c08c747ea8a8f173c46c9
 class School(models.Model):
     name_school = models.CharField(max_length=255)
     address_school = models.TextField()
@@ -57,44 +30,23 @@ class School(models.Model):
         return f'{self.name_school}'
 
 class Subject(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
     photo_subject = models.ImageField(upload_to='photo_profile/')
     subject_name = models.CharField(max_length=255)
 
     def __str__(self):
         return f'{self.subject_name}'
 
-class Teacher(models.Model):
-    teacher_name = models.CharField(max_length=64)
-<<<<<<< HEAD
-<<<<<<< HEAD
-    subject_teacher = models.ForeignKey(Subject, on_delete=models.CASCADE)
-=======
->>>>>>> a3f4685408f1182b68358eb3169d9d32d815c96d
-=======
-    subject_teacher = models.ForeignKey(Subject, on_delete=models.CASCADE)
->>>>>>> d8dbb0b8e4e4a42dcd5c08c747ea8a8f173c46c9
-
-    def __str__(self):
-        return f'{self.teacher_name}'
-
 class ClassGroup(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
     class_name = models.CharField(max_length=155, unique=True)
     class_year = models.CharField(max_length=12)
 
     def __str__(self):
         return f'{self.class_name}'
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-=======
->>>>>>> a3f4685408f1182b68358eb3169d9d32d815c96d
-=======
-
-
->>>>>>> d8dbb0b8e4e4a42dcd5c08c747ea8a8f173c46c9
 class StudentProfile(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
     user_student = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
     class_group = models.ForeignKey(ClassGroup, on_delete=models.CASCADE)
 
@@ -102,25 +54,26 @@ class StudentProfile(models.Model):
         return f'{self.user_student}'
 
 class Lesson(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     class_group = models.ForeignKey(ClassGroup, on_delete=models.CASCADE)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     def __str__(self):
         return f'{self.subject.subject_name}'
 
-=======
->>>>>>> a3f4685408f1182b68358eb3169d9d32d815c96d
-=======
-    def __str__(self):
-        return f'{self.subject.subject_name}'
+class Teacher(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    teacher_name = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    subject_teacher = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
->>>>>>> d8dbb0b8e4e4a42dcd5c08c747ea8a8f173c46c9
+    def __str__(self):
+        return f'{self.teacher_name}'
+
+
 class Grade(models.Model):
     student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
@@ -130,37 +83,21 @@ class Grade(models.Model):
     ('3', '3'),
     ('4', '4'),
     ('5', '5'),
-<<<<<<< HEAD
-<<<<<<< HEAD
-    ('H', 'H'),
-    ('Нб', 'Нб')
-=======
->>>>>>> a3f4685408f1182b68358eb3169d9d32d815c96d
-=======
-    ('H', 'H'),
-    ('Нб', 'Нб')
->>>>>>> d8dbb0b8e4e4a42dcd5c08c747ea8a8f173c46c9
+    ('н', 'н'),
+    ('нб', 'нб')
     )
-    value_choices = models.CharField(max_length=10, choices=VALUE_CHOICES)
+    value_choices = models.CharField(max_length=10, choices=VALUE_CHOICES, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     def __str__(self):
-        return f'{self.student.full_name}'
+        return f'{self.student.user_student.full_name}'
 
-=======
->>>>>>> a3f4685408f1182b68358eb3169d9d32d815c96d
-=======
-    def __str__(self):
-        return f'{self.student.full_name}'
-
->>>>>>> d8dbb0b8e4e4a42dcd5c08c747ea8a8f173c46c9
     def predicted_quarter_grade(self):
         grades = Grade.objects.filter(student=self.student, subject=self.subject)
         if not grades.exists():
             return 0
         return round(sum(g.value_choices for g in grades) / grades.count(), 2)
+
 
 class QuarterGrade(models.Model):
     student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE)
@@ -172,45 +109,23 @@ class QuarterGrade(models.Model):
     ('5', '5'),
     )
     quarter_choices = models.CharField(max_length=10, choices=QUARTER_CHOICES)
-<<<<<<< HEAD
-<<<<<<< HEAD
+    QUARTER_NUMBER = (('I', 'I'), ('II', 'II'), ('III', 'III'), ('IV', 'IV'))
+    quarter_number = models.CharField(max_length=10, choices=QUARTER_NUMBER, default='1')
+    school_year = models.CharField(max_length=10)
 
     def __str__(self):
-        return f'{self.student.full_name}'
-=======
-    final_grade = models.IntegerField()
->>>>>>> a3f4685408f1182b68358eb3169d9d32d815c96d
-=======
-
-    def __str__(self):
-        return f'{self.student.full_name}'
->>>>>>> d8dbb0b8e4e4a42dcd5c08c747ea8a8f173c46c9
+        return f'{self.student.user_student.full_name}'
 
 class Homework(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     file_url = models.URLField(blank=True, null=True)
-<<<<<<< HEAD
-<<<<<<< HEAD
     deadline = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.title}'
 
-=======
-    deadline = models.DateField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
->>>>>>> a3f4685408f1182b68358eb3169d9d32d815c96d
-=======
-    deadline = models.DateField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.title}'
-
->>>>>>> d8dbb0b8e4e4a42dcd5c08c747ea8a8f173c46c9
 
 class Book(models.Model):
     title = models.CharField(max_length=150)
@@ -219,45 +134,30 @@ class Book(models.Model):
     grade_level = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(11)])
     file_url = models.URLField()
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> d8dbb0b8e4e4a42dcd5c08c747ea8a8f173c46c9
     def __str__(self):
         return f'{self.title}'
 
-class Message(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    message = models.TextField()
-    file = models.FileField(upload_to='file/')
-    voice = models.FileField(upload_to='voices/')
-    sticker = models.FileField(upload_to='stickers/')
-    message_date = models.DateTimeField(auto_now_add=True)
 
-class Chat(models.Model):
-    admin = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='admin')
-    user = models.ManyToManyField(UserProfile, related_name='user')
-    group_name = models.CharField(max_length=64)
-    group_image = models.ImageField(upload_to='group_image/')
-    class_chat = models.ForeignKey(ClassGroup, on_delete=models.CASCADE)
-    create_group = models.DateTimeField(auto_now_add=True)
+class Attendance(models.Model):
+    STATUS_CHOICES=(
+    ("present","present"),
+    ("absent","absent"),
+    )
+    student = models.ForeignKey(StudentProfile,on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson,on_delete=models.CASCADE)
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    absent_time = models.DateTimeField(null=True, blank=True)
+    present_time = models.DateTimeField(null=True, blank=True)
+    late_minutes = models.IntegerField(default=0)
 
-
-<<<<<<< HEAD
-=======
->>>>>>> a3f4685408f1182b68358eb3169d9d32d815c96d
-=======
->>>>>>> d8dbb0b8e4e4a42dcd5c08c747ea8a8f173c46c9
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def save(self, *args, **kwargs):
+        if self.status == "absent" and not self.absent_time:
+            self.absent_time = timezone.now()
+        if self.status == "present" and self.absent_time and not self.present_time:
+            self.present_time = timezone.now()
+            diff = self.present_time - self.absent_time
+            self.late_minutes = int(diff.total_seconds() // 60)
+        super().save(*args, **kwargs)
+    def __str__(self):
+        return f'{self.student.user_student}'
